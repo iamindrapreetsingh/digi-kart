eb13a15da27942f684ff116c32a750c504a55bee
ashhadimam07 <ashhadimam07@gmail.com>
2021-03-20 12:50:44 +0530

    Modify pageSize

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,16 +3,16 @@ import Product from '../models/productModel.js'
 
 
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 10
+  const pageSize = 8
   const page = Number(req.query.pageNumber) || 1
 
   const keyword = req.query.keyword
     ? {
-        name: {
-          $regex: req.query.keyword,
-          $options: 'i',
-        },
-      }
+      name: {
+        $regex: req.query.keyword,
+        $options: 'i',
+      },
+    }
     : {}
 
   const count = await Product.countDocuments({ ...keyword })
