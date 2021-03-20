f45913e7430dff92b348db56a9e95c305bb47283
ashhadimam07 <ashhadimam07@gmail.com>
2021-03-20 13:44:54 +0530

    Change error message

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -108,7 +108,7 @@ const ProductScreen = ({ history, match }) => {
                   )}
                   {loadingProductReview && <Loader />}
                   {errorProductReview && (
-                    <Message variant='danger'>{errorProductReview}</Message>
+                    <Message variant='danger'>Product already reviewed</Message>
                   )}
                   {userInfo ? (
                     <Form onSubmit={submitHandler}>
